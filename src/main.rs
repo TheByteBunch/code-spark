ab6e87c7e0a7cf8bc9833ef99d65fa2224735fa3
Erik K Anderson <erik@19bit.net>
2023-08-16 11:30:55 -0500

    implement create user and list users by http

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-#[macro_use] extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+extern crate rocket;
+use rocket::{launch, routes};
+use rocket_dyn_templates::{ Template };
+mod services;
+pub mod  models;
+pub mod  schema;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![services::create_user])
+        .mount("/", routes![services::list])
+        .attach(Template::fairing())
 }
