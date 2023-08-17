02442fa996a61a0162308c2fd53d94d7c652920d
Erik K Anderson <erik@19bit.net>
2023-08-17 03:21:25 -0500

    fresh start

--- migrations/2023-08-16-154540_users/up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    user_email VARCHAR(255) NOT NULL,
-    user_full_name VARCHAR(255) NOT NULL,
-    user_github_profile_name VARCHAR(255) NOT NULL
-);
